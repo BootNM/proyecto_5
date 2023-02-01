80e34577b3c9de1e991883ec055cfa73c18ab0b3
BootNM <nimeneses.dwfsc2@bootcampudd.cl>
2023-01-31 22:31:24 -0300

    navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,6 +4,7 @@ import NavbarBT from 'react-bootstrap/Navbar';
 import ShoppingCartContext from '../../contexts/shopping-cart/ShoppingCartContext.jsx';
 import UserContext from '../../contexts/users/UserContext.jsx';
 import ShoppingCart from '../ShoppingCart/ShoppingCart.jsx';
+import logo from '../../img/logo.webp'
 
 import './navbar.css';
 
@@ -20,29 +21,32 @@ export default function Navbar() {
   const handleShowShoppingCart = () => setShowShoppingCart( true );
   return (
     <>
-      <NavbarBT bg='dark' variant='dark' expand="md">
+      <NavbarBT className="navbar" variant='dark' expand="md">
         <Container>
-          <NavbarBT.Brand href='#'>
-            <img src='https://react-bootstrap.github.io/logo.svg' width="30" height="30" className="d-inline-block align-top"
+          <NavbarBT.Brand href='/'>
+            <img src={logo} width="50" height="50" borderRadius="0"  className="d-inline-block align-top"
               alt="React Bootstrap logo"></img>
           </NavbarBT.Brand>
-          <NavbarBT.Toggle aria-controls="basic-navbar-nav"></NavbarBT.Toggle>
-          <NavbarBT.Collapse id='basic-navbar-nav'>
+          <Container className="titulo d-block">
+            <h1>Segunda Piel</h1>
+          </Container>
+          <NavbarBT.Toggle  className="botonera" aria-controls="basic-navbar-nav"></NavbarBT.Toggle>
+          <NavbarBT.Collapse className="botonera" id='basic-navbar-nav'>
             <Nav>
-              <Button><Nav.Link href='/'>Home</Nav.Link></Button>
+              <Button className='btn-nav'><Nav.Link href='/'>Home</Nav.Link></Button>
 
               {
                 user?.email ? <>
-                  <Button><Nav.Link href='/profile'>Perfil</Nav.Link></Button>
-                  <Button onClick={ () => logout() } > <Nav.Link href='/'>Cerrar sesión</Nav.Link></Button>
+                  <Button className='btn-nav'><Nav.Link href='/profile'>Perfil</Nav.Link></Button>
+                  <Button className='btn-nav' onClick={ () => logout() } > <Nav.Link href='/'>Cerrar sesión</Nav.Link></Button>
                   <Button variant="primary" onClick={ handleShowShoppingCart }>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-cart" viewBox="0 0 16 16">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="btn-nav bi bi-cart" viewBox="0 0 16 16">
                       <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
                     </svg> <Badge bg="secondary">{ products.length > 0 ? products.length : '' }</Badge>
                   </Button>
                 </> : <>
-                  <Button><Nav.Link href='/auth/login'>Ingreso</Nav.Link></Button>
-                  <Button><Nav.Link href='/auth/signup'>Registro</Nav.Link></Button>
+                  <Button className='btn-nav'><Nav.Link href='/auth/login'>Ingreso</Nav.Link></Button>
+                  <Button  className='btn-nav'><Nav.Link href='/auth/signup'>Registro</Nav.Link></Button>
                 </>
               }
             </Nav>
